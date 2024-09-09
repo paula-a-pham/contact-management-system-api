98625b39e4966ecfa7f7fdaab47822ee58fb033d
Paula Ashraf <paula.a.pham@gmail.com>
2024-09-09 11:56:37 +0300

    Create contact service implementation

--- BusinessLogic/Service/Implementation/ContactService.cs
+++ BusinessLogic/Service/Implementation/ContactService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusinessLogic.Service.Interfaces
+namespace BusinessLogic.Service.Implementation
 {
-    public interface IContact
+    public class ContactService
     {
     }
 }
