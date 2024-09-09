4d1b9c4a62055396bfbafb83a691696000d46f9b
Paula Ashraf <paula.a.pham@gmail.com>
2024-09-09 11:40:04 +0300

    Create contact category model

---
+++ Data/Models/ContactCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class ContactCategory
+    {
+        public int ContactCategoryId { get; set; }
+        [MaxLength(255)]
+        public required string Name { get; set; }
+    }
+}
