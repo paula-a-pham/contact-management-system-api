10c20f4ccd81ea886a4ddd07a08cc1ed3a0e1544
Paula Ashraf <paula.a.pham@gmail.com>
2024-09-09 12:03:10 +0300

    Create async method in contact service for fetching all contacts

--- BusinessLogic/Service/Implementation/ContactService.cs
+++ BusinessLogic/Service/Implementation/ContactService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BusinessLogic.Service.Interfaces;
+using Data;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,18 @@ using System.Threading.Tasks;
 
 namespace BusinessLogic.Service.Implementation
 {
-    public class ContactService
+    public class ContactService : IContactService
     {
+        private readonly ApplicationDbContext _context;
+
+        public ContactService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<Contact>> GetAllContactsAsync()
+        {
+            return await _context.Contacts.Include(c => c.ContactCategory).ToListAsync();
+        }
     }
 }
--- BusinessLogic/Service/Interfaces/IContactService.cs
+++ BusinessLogic/Service/Interfaces/IContactService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace BusinessLogic.Service.Interfaces
 {
     public interface IContactService
     {
+        Task<ICollection<Contact>> GetAllContactsAsync();
     }
 }
