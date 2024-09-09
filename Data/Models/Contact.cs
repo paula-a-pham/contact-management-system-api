f514f1c653c489935444baafe11337ffe81aeee8
Paula Ashraf <paula.a.pham@gmail.com>
2024-09-09 11:37:39 +0300

    Create contact model

---
+++ Data/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        [MaxLength(100)]
+        public required string FirstName { get; set; }
+        [MaxLength(100)]
+        public string? LastName { get; set; }
+        [MaxLength(15)]
+        public required string PhoneNumber { get; set; }
+        [MaxLength(255)]
+        public string? EmailAddress { get; set; }
+        public string? Image { get; set; }
+    }
+}
