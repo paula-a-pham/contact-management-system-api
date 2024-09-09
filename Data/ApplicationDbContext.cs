4514128d7075ed8dce5d0827f42e32f290d50584
Paula Ashraf <paula.a.pham@gmail.com>
2024-09-09 11:46:19 +0300

    Define contact and contact category as a domain models in application context

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,7 +1,15 @@
-﻿namespace Data
-{
-    public class ApplicationDbContext
-    {
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore;
 
+namespace Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<ContactCategory> ContactCategories { get; set; }
     }
 }
